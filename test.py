dadc3f074bcf9627a9ddaaf053c25efb8eaac535
ahmedshahkhan <askshahkhan@gmail.com>
2025-09-07 12:49:28 -0500

    able to write text to notion page

--- test.py
+++ test.py
@@ -7,12 +7,32 @@ load_dotenv()
 notion_token = os.getenv('NOTION_TOKEN')
 notion_page_id = os.getenv('PAGE_ID')
 
+def write_text(client, page_id, text, type):
+    client.blocks.children.append(
+        block_id=page_id,
+        children=[
+            {
+                "object": "block",
+                "type": type,
+                type: {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": text
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
+    )
+
 def main():
     client = Client(auth=notion_token)
 
-    page_response = client.pages.retrieve(notion_page_id)
-
-    pprint(page_response, indent=2)
+    while True:
+        write_text(client, notion_page_id, 'Hello World!', 'to_do')
 
 if __name__ == '__main__':
     main()
